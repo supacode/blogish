fa82b8755d5ded09ac864baced0c6679a1ff70e3
Maverick <bredren10@gmail.com>
2019-11-04 02:35:46 +0100

    Prevent paramater pollution

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const hpp = require('hpp');
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/post');
@@ -17,6 +18,9 @@ const app = express();
 
 app.use(express.json());
 
+// Prevent parameter pollution
+app.use(hpp());
+
 // Helmet
 app.use(helmet());
 
@@ -28,7 +32,7 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/comment', commentRoutes);
 
-// Fallback
+// Fallback URL
 app.all('*', (req, res, next) => {
   res.status(404).json({
     status: 'fail',
--- utils/connectDB.js
+++ utils/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  await mongoose.connect('mongodb://127.0.0.1:27017/blogish', {
+  await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
